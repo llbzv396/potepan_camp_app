b98619f395eda3f53832bc429a78f18bb69beca6
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-22 02:38:20 +0900

    display product image on show page

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,10 +1,10 @@
-class Potepan::ProductsController < ApplicationController
-
-  def show
-    # Spree::Productから製品に関する情報を取得
-    @product = Spree::Product.find(params[:id])
-    # Spree::ProductPropertyとSpree::Propertyから製品の詳細な情報を取得
-    @product_properties = @product.product_properties.includes(:property)
+  class Potepan::ProductsController < ApplicationController
+    def show
+      # Spree::Productから製品に関する情報を取得
+      @product = Spree::Product.find(params[:id])
+      # Spree::ProductPropertyとSpree::Propertyから製品の詳細な情報を取得
+      @product_properties = @product.product_properties.includes(:property)
+      # 画像を表示させるためのデータを取得
+      @product_images = @product.images
+    end
   end
-
-end

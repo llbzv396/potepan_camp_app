95db12dfe2b75e098a1a7c8b5ca2462863b7cccc
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-22 18:20:47 +0900

    add first rspec test

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
 
+    let(:product) { FactoryGirl.create(:product) }
+
+    describe "showアクションに関するテスト" do
+      it "getアクションが反応するか" do
+        get :show, params: { id: product.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
 end

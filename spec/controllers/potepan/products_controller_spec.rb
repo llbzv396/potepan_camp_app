dcbc19caacec4d4ef622e42bb03c77f430d1a1a4
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-24 22:26:35 +0900

    Modified rspec setting

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
 
-    let(:product) { FactoryGirl.create(:product) }
+    let(:product) { create(:product) }
 
     describe "showアクションに関するテスト" do
-      it "getアクションが反応するか" do
+      it "リクエストが成功し、レスポンスは成功しているか" do
         get :show, params: { id: product.id}
         expect(response).to have_http_status(:success)
       end

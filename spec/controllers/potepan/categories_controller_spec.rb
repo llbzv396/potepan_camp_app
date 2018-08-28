525f7021ef194d5c694b5b230bb149e1da439e4f
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-28 16:16:17 +0900

    add categories controller test

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
 
+  let(:taxon) { create(:taxon) }
+  let(:taxonomy) { create(:taxonomy) }
+
+  describe "showアクションに関するテスト" do
+
+    it "正常にレスポンスを返すこと" do
+      get :show, params: { id: taxon.id }
+      expect(response).to be_success
+    end
+
+    it "ステータスコードが200のレスポンスを返すこと" do
+      get :show, params: { id: taxon.id }
+      expect(response). to have_http_status "200"
+    end
+
+    it "showアクション内の'@taxons'と作成した'taxon'が等しいか" do
+      get :show, params: { id: taxon.id }
+      puts assigns(:taxons).name
+      puts taxon.name
+      expect(assigns(:taxons)).to eq taxon
+    end
+
+  end
+
 end

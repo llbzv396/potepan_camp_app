00078ec514bd636d7c7a20415afc4d2297e5f3af
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-28 15:50:52 +0900

    Modified products partial

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,6 +3,11 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxons = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all
+    if @taxons.root?
+      @products = @taxons.leaves
+    else
+      @products = @taxons.products
+    end
   end
 
 end

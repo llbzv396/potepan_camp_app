4d5016ffbf38ff627ea23351a5cbac78831ccfc6
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-31 12:44:45 +0900

    refactoring variable name @taxons => @taxon

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,10 +1,10 @@
 class Potepan::CategoriesController < ApplicationController
 
   def show
-    @taxons = Spree::Taxon.find(params[:id])
+    @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all
     # @taxons自信+以下のカテゴリ全てのidを配列で返す => それで検索をかける
-    @products = Spree::Product.in_taxon(@taxons)
+    @products = Spree::Product.in_taxon(@taxon)
   end
 
 end

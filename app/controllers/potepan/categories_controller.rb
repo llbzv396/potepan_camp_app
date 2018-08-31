616917fdc9f761ee0a2070aed0585a0be7eb6ded
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-31 17:34:21 +0900

    add .includes categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,9 +2,9 @@ class Potepan::CategoriesController < ApplicationController
 
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxonomy.all
+    @taxonomies = Spree::Taxonomy.all.includes(:root)
     # @taxons自信+以下のカテゴリ全てのidを配列で返す => それで検索をかける
-    @products = Spree::Product.in_taxon(@taxon)
+    @products = Spree::Product.includes(master: [:images]).in_taxon(@taxon)
   end
 
 end

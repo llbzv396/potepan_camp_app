b2ef6ea28e34ea3f00bd92d1dd259a0441894a87
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-31 23:29:43 +0900

    add includes.(default_price) in categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,10 +1,8 @@
 class Potepan::CategoriesController < ApplicationController
-
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
     # @taxons自信+以下のカテゴリ全てのidを配列で返す => それで検索をかける
-    @products = Spree::Product.includes(master: [:images]).in_taxon(@taxon)
+    @products = Spree::Product.includes(master: [:images, :default_price]).in_taxon(@taxon)
   end
-
 end

67b5d4093ad83cf66e6c5f216f5dd69152fb654e
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-08-31 12:25:37 +0900

    add root categories detail info

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,11 +3,8 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxons = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all
-    if @taxons.root?
-      @products = @taxons.leaves
-    else
-      @products = @taxons.products
-    end
+    # @taxons自信+以下のカテゴリ全てのidを配列で返す => それで検索をかける
+    @products = Spree::Product.in_taxon(@taxons)
   end
 
 end

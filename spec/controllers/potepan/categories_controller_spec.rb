065485a2ce20858bae6a6161c0560501823ebe8b
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-01 01:04:37 +0900

    add categories_controller test

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
   let(:taxon) { create(:taxon) }
-  let(:taxonomy) { create(:taxonomy) }
+  let(:products) do
+    create_list(:product, 10) do |product|
+      product.taxons << taxon
+    end
+  end
 
   describe "showアクションに関するテスト" do
     it "正常にレスポンスを返すこと" do
@@ -17,9 +21,12 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
 
     it "showアクション内の'@taxons'と作成した'taxon'が等しいか" do
       get :show, params: { id: taxon.id }
-      puts assigns(:taxons).name
-      puts taxon.name
-      expect(assigns(:taxons)).to eq taxon
+      expect(assigns(:taxon)).to eq taxon
+    end
+
+    it "showアクション内の'@products'と作成した'products'が等しいか" do
+      get :show, params: { id: taxon.id }
+      expect(assigns(:products)).to eq products
     end
   end
 end

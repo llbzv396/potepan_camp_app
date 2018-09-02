004d77849280fa684fe3788489c864301f9636bc
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-02 16:38:03 +0900

    Modified products_controller

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,15 +1,11 @@
 class Potepan::ProductsController < ApplicationController
   def show
-    # Spree::Productから製品に関する情報を取得
     @product = Spree::Product.friendly.find(params[:id])
-    # Spree::ProductPropertyとSpree::Propertyから製品の詳細な情報を取得
     @product_properties = @product.product_properties.includes(:property)
-    # 画像を表示させるためのデータを取得
     @product_images = @product.images
-    # 表示している商品のカテゴリを取得
-    @taxon = @product.taxons.where(taxonomy_id: 1)
-    # そのカテゴリのtaxon.idを使ってclassificationから商品の一覧を検索
-    @related = Spree::Classification.where(taxon_id: @taxon.ids)
-    @related_products = Spree::Product.where(id: @related.all.map(&:product_id))
+    @related_products = Spree::Product.joins(:taxons).
+      includes(master: [:images, :default_price]).
+      where(spree_taxons: { id: @product.taxons.ids }).
+      where.not(id: @product.id).distinct
   end
 end

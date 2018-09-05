3e9dc768e9b77595ffb7553131be8a16eb14a3ce
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-06 05:40:00 +0900

    add rspec test

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -3,6 +3,21 @@ require 'rails_helper'
 RSpec.describe Potepan::ProductsController, type: :controller do
   let(:product) { create(:base_product) }
 
+  describe "indexアクションに関するテスト" do
+    it "正常にレスポンスを返すこと" do
+      get :index
+      expect(response).to be_success
+    end
+
+    it "ステータスコードが200のレスポンスを返すこと" do
+      get :index
+      expect(response).to have_http_status "200"
+    end
+
+    it "新着商品が表示されていること" do
+    end
+  end
+
   describe "showアクションに関するテスト" do
     it "正常にレスポンスを返すこと" do
       get :show, params: { id: product.id }

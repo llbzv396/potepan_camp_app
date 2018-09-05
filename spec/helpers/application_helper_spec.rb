232a8ad30572198dcdfe07827d65acccede5060d
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-05 10:25:17 +0900

    Modified ApplicationHelperTest

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,13 +5,17 @@ RSpec.describe ApplicationHelper do
   let(:page_title) { 'Ruby on Rails' }
 
   describe "full_titleメソッドに関するテスト" do
-    it "page_titleが空の場合" do
-      page_title = ''
-      expect(full_title(page_title)).to eq "BIGBAG Store"
+    context "page_titleが空の場合" do
+      it "full_titleが[BIGBAG Store]となる事" do
+        page_title = ''
+        expect(full_title(page_title)).to eq "BIGBAG Store"
+      end
     end
 
-    it "page_titleがRuby on Railsの場合" do
-      expect(full_title(page_title)).to eq "Ruby on Rails - BIGBAG Store"
+    context "page_titleがRuby on Railsの場合" do
+      it "full_titleが[Ruby on Rails - BIGBAG Store]となる事" do
+        expect(full_title(page_title)).to eq "Ruby on Rails - BIGBAG Store"
+      end
     end
   end
 end

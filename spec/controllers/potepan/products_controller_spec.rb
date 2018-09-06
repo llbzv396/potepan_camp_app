3585333cb85b0c35d69de8d6ae5025fb2b73d0e2
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-06 18:49:31 +0900

    Refactored the rspec

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe Potepan::ProductsController, type: :controller do
   let(:product) { create(:base_product) }
 
+  before do
+    get :show, params: { id: product.id }
+  end
+
   describe "showアクションに関するテスト" do
     it "正常にレスポンスを返すこと" do
-      get :show, params: { id: product.id }
       expect(response).to be_success
     end
 
     it "ステータスコードが200のレスポンスを返すこと" do
-      get :show, params: { id: product.id }
       expect(response).to have_http_status "200"
     end
 
-    it "showアクション内の'@product'と作成した'product'が等しいか " do
-      get :show, params: { id: product.id }
+    it "showアクション内の'@product'と作成した'product'が等しいか " do      
       expect(assigns(:product)).to eq product
     end
   end

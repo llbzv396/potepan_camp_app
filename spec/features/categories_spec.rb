911d293bcb0291c15774a66f65e283ed08d01684
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-08 14:57:01 +0900

    Modified feature/categories_spec

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.feature "Categories", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:taxonomy) { create(:taxonomy) }
+  let(:root_taxon) do
+    create(:taxon, taxonomy_id: taxonomy.id,
+                   id: 1,
+                   parent_id: nil)
+  end
+  let!(:child_taxon1) do
+    create(:taxon, taxonomy_id: taxonomy.id,
+                   parent_id: root_taxon.id,
+                   name: "child_taxon1")
+  end
+  let!(:child_taxon2) do
+    create(:taxon, taxonomy_id: taxonomy.id,
+                   parent_id: root_taxon.id,
+                   name: "child_taxon2")
+  end
+  let!(:product1) { create(:base_product, taxons: [root_taxon, child_taxon1]) }
+  let!(:product2) { create(:base_product, taxons: [root_taxon, child_taxon2]) }
+
+  scenario "categories/show に表示されている商品一覧を確認する" do
+    visit potepan_category_path(root_taxon.id)
+    expect(page).to have_content root_taxon.name
+    expect(page).to have_content taxonomy.name
+    expect(page).to have_content child_taxon1.name
+    expect(page).to have_content child_taxon2.name
+    expect(page).to have_content product1.name
+    expect(page).to have_content product2.name
+  end
+
+  scenario "categories/show のサイドバーのリンクを確認する" do
+    visit potepan_category_path(root_taxon.id)
+    click_on "#{child_taxon1.name}"
+    expect(page).to have_content root_taxon.permalink
+    expect(page).to have_content taxonomy.name
+    expect(page).to have_content child_taxon1.name
+    expect(page).to have_content child_taxon2.name
+    expect(page).to have_content product1.name
+    expect(page).not_to have_content product2.name
+    expect(page).to have_current_path(potepan_category_path(child_taxon1.id))
+  end
+
+  scenario "categories/show に表示されている商品ページのリンクをクリックする" do
+    visit potepan_category_path(root_taxon.id)
+    click_on "#{product1.name}"
+    expect(page).to have_content product1.name
+    expect(page).to have_content product1.price
+    expect(page).to have_content product1.description
+    expect(page).to have_content "Return to list"
+    expect(page).to have_current_path(potepan_product_path(product1))
+  end
 end

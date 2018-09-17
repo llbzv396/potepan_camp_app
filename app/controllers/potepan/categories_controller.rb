bd3309990706241d8495a174b2d70b85129ff9ac
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-17 23:50:09 +0900

    Add filter_partials

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,5 +3,6 @@ class Potepan::CategoriesController < ApplicationController
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
     @products = Spree::Product.including_images_prices.in_taxon(@taxon)
+    @options = Spree::OptionType.all.includes(:option_values)
   end
 end

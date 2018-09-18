3cdcc64063c16de55a8597a43b0d175c3298f32a
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-19 03:00:36 +0900

    Add COUNT_OF_LATED_PRODUCTS

--- app/controllers/potepan/home_controller.rb
+++ app/controllers/potepan/home_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::HomeController < ApplicationController
+  COUNT_OF_LATED_PRODUCTS = 5
   def index
-    @lated_products = Spree::Product.including_images_prices.order(available_on: :desc).limit(5)
+    @lated_products = Spree::Product.including_images_prices.order(available_on: :desc).limit(COUNT_OF_LATED_PRODUCTS)
   end
 end

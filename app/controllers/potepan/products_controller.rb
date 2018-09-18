9ef4b941270621b524d0019a8ef8f97ad316a76a
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-18 13:35:53 +0900

    Add COUNT_OF_RELATED_PRODUCTS

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,9 +1,10 @@
 class Potepan::ProductsController < ApplicationController
+  COUNT_OF_RELATED_PRODUCTS = 4
   def show
     @product = Spree::Product.friendly.find(params[:id])
     @product_properties = @product.product_properties.includes(:property)
     @product_images = @product.images
     @related_products = Spree::Product.related_products(@product).
-      including_images_prices.distinct.limit(4)
+      including_images_prices.distinct.limit(COUNT_OF_RELATED_PRODUCTS)
   end
 end

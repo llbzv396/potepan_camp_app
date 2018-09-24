ad1df5cd432896a1e598e4d28f47fa97fc28f122
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-09-25 02:55:36 +0900

    Add system of filter_by_color

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,8 +1,34 @@
 class Potepan::CategoriesController < ApplicationController
+  helper_method :option_value, :products_count_filtered_by_color
+
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
-    @products = Spree::Product.including_images_prices.in_taxon(@taxon)
     @options = Spree::OptionType.all.includes(:option_values)
+    if params[:color].present?
+      @products = Spree::Product.
+        in_taxon(@taxon).
+        filter_by_color(option_value(params[:color]).name, option_type(params[:type]).id).
+        including_images_prices
+    else
+      @products = Spree::Product.including_images_prices.in_taxon(@taxon)
+    end
   end
+
+  private
+
+  def option_value(value)
+    Spree::OptionValue.find(value)
+  end
+
+  def option_type(type)
+    Spree::OptionType.find(type)
+  end
+
+  def products_count_filtered_by_color(value, type)
+    Spree::Product.
+    in_taxon(@taxon).
+    filter_by_color(value, type).ids.uniq.count
+  end
+
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,5 +2,11 @@ Spree::Product.class_eval do
   scope :related_products, ->(product) {
     joins(:taxons).where(spree_taxons: { id: product.taxons.ids }).where.not(id: product.id)
   }
-  scope :including_images_prices, -> { includes(master: [:images, :default_price]) }
+  scope :including_images_prices, -> {
+    includes(master: [:images, :default_price])
+  }
+  scope :filter_by_color, ->(value, type) {
+    joins(variants_including_master: :option_values).
+      where("spree_option_values.name = ? AND spree_option_values.option_type_id = ?", value, type)
+  }
 end

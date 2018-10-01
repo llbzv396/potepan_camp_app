35cf1ed5cddd29b9dd315c35a618ab28d8a147a7
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-10-01 18:08:04 +0900

    Add test item of product_properties & properties

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
-  let(:product) { create(:base_product) }
+  let(:property) { create(:property) }
+  let(:product_property) { create(:product_property, property: property) }
+  let(:product) { create(:product, product_properties: [product_property]) }
 
   describe "showアクションに関するテスト" do
-    it "正常にレスポンスを返すこと" do
+    before do
       get :show, params: { id: product.id }
+    end
+
+    it "正常にレスポンスを返すこと" do
       expect(response).to have_http_status(:ok)
     end
 
-    it "showアクション内の'@product'と作成した'product'が等しいか" do
-      get :show, params: { id: product.id }
+    it "適切なテンプレートが表示されているか" do
+      expect(response).to render_template(:show)
+    end
+
+    it "@productは適切な情報を持つか" do
       expect(assigns(:product)).to eq product
+    end
+
+    it "@product_propertiesは適切な情報を持つか" do
+      expect(assigns(:product_properties)).to eq product.product_properties
     end
   end
 end

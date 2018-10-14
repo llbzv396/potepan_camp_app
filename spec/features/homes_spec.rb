d595243e7257ccc8ed2f0ce58ac527687a1e893b
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-10-14 12:04:40 +0900

    Modified home_specs

--- spec/features/homes_spec.rb
+++ spec/features/homes_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.feature "Homes", type: :feature do
   let!(:root_taxon) { create(:taxon) }
   let!(:lated_product) do
-    create(:product, name: "lated_product",
+    create(:product, name: 'lated_product',
                      available_on: 1.month.ago,
                      price: 20.24,
+                     description: 'This is lated_product',
                      taxons: [root_taxon])
   end
   let!(:basis_product) do
     create_list(:product, 7,
-                name: "basis_product",
+                name: 'basis_product',
                 available_on: 4.month.ago,
                 price: 17.23)
   end
   let!(:old_product) do
-    create(:product, name: "old_product",
+    create(:product, name: 'old_product',
                      available_on: 1.years.ago,
                      price: 15.32)
   end
@@ -25,18 +26,20 @@ RSpec.feature "Homes", type: :feature do
   end
 
   scenario "ホームに表示されている内容を確認する" do
-    expect(page).to have_content lated_product.name
-    expect(page).to have_content lated_product.price
-    expect(page).not_to have_content old_product.name
-    expect(page).not_to have_content old_product.price
+    expect(page).to have_content 'lated_product'
+    expect(page).to have_content 20.24
+    expect(page).to have_content 'basis_product'
+    expect(page).to have_content 17.23
+    expect(page).not_to have_content 'old_product'
+    expect(page).not_to have_content 15.32
   end
 
   scenario "新着商品のリンクをクリックする" do
-    click_on lated_product.name
+    click_on 'lated_product'
     expect(page).to have_current_path(potepan_product_path(lated_product))
-    expect(page).to have_content lated_product.name
-    expect(page).to have_content lated_product.price
-    expect(page).to have_content lated_product.description
-    expect(page).to have_content "Return to list"
+    expect(page).to have_content 'lated_product'
+    expect(page).to have_content 20.24
+    expect(page).to have_content 'This is lated_product'
+    expect(page).to have_content 'Return to list'
   end
 end

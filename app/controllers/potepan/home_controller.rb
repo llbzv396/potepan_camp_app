0c499a2072d2f0a2721a550b9a204b8688af40e7
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-10-23 00:13:02 +0900

    Modified popular_category (add links)

--- app/controllers/potepan/home_controller.rb
+++ app/controllers/potepan/home_controller.rb
@@ -3,5 +3,9 @@ class Potepan::HomeController < ApplicationController
   def index
     @lated_products = Spree::Product.including_images_prices.
       order(available_on: :desc).limit(COUNT_OF_LATED_PRODUCTS)
+
+    @shirts = Spree::Taxon.find_by(name: "Shirts")
+    @bags = Spree::Taxon.find_by(name: "Bags")
+    @mugs = Spree::Taxon.find_by(name: "Mugs")
   end
 end

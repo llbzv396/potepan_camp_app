92015348a2b18aa39f818f66b3db71336324e508
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-11-01 10:32:03 +0900

    Modified grid-list switching

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,11 +1,13 @@
 class Potepan::CategoriesController < ApplicationController
   helper_method :option_value, :products_count_filtered_by_color
+  before_action :set_view_type, only: [:show]
 
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
     @options = Spree::OptionType.all.includes(:option_values)
-    if params[:color].present?
+
+    if filtered_by_color?
       @products = Spree::Product.
         in_taxon(@taxon).
         filter_by_color(option_value(params[:color]).name, option_type(params[:type]).id).
@@ -29,5 +31,17 @@ class Potepan::CategoriesController < ApplicationController
     Spree::Product.
       in_taxon(@taxon).
       filter_by_color(value, type).ids.uniq.count
+  end
+
+  def filtered_by_color?
+    params[:color].present?
+  end
+
+  def set_view_type
+    if params[:format] == 'list'
+      session[:format] = 'list'
+    elsif params[:format] == 'grid'
+      session[:format] = 'grid'
+    end
   end
 end

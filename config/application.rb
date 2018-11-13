a1ed72ab0f52a1d0b38c0dc1ab0e4f8129b0aa03
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-11-13 21:17:42 +0900

    Setup for heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,19 @@ module Potepanec
       end
     end
 
+
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -48,5 +61,6 @@ module Potepanec
     end
 
     config.time_zone = 'Tokyo'
+    config.assets.enabled = true
   end
 end

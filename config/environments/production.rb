c98e04c08698cafeac10e43eac1ae2ef04ff6443
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-11-15 01:23:35 +0900

    add setting for font-awesome

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   config.serve_static_files = true
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -25,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
